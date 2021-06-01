74495e729f562f827815855433053e76b94c0056
0xantman <antho74830@gmail.com>
2021-06-01 12:53:23 +0200

    work in progress...

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import {
+    useConnectedWallet,
+    ConnectedWallet,
+    useWallet
+} from '@terra-money/wallet-provider';
 
 export default function ConnectWallet(){
+    const connectedWallet = ConnectedWallet ? useConnectedWallet() : undefined;
+    const {
+        status,
+        network,
+        availableConnectTypes,
+        connect,
+        availableInstallTypes,
+        install,
+        wallets,
+        disconnect,
+        recheckStatus,
+        post,
+    } = useWallet();
     return(
         <button>Connect Wallet</button>
     )
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,24 +1,5 @@
 import React from "react";
-import {
-    useConnectedWallet,
-    ConnectedWallet,
-    useWallet
-} from '@terra-money/wallet-provider';
 
 export default function Index () {
-     const connectedWallet = ConnectedWallet ? useConnectedWallet() : undefined;
-    const {
-        status,
-        network,
-        availableConnectTypes,
-        connect,
-        availableInstallTypes,
-        install,
-        wallets,
-        disconnect,
-        recheckStatus,
-        post,
-    } = useWallet();
-     console.log(status)
-     return (<h1>Hello World {connectedWallet}</h1>);
+     return (<h1>Hello World</h1>);
 }
