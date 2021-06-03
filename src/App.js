4252df1df44c05ef7c19c07ee9ff3608869d98d8
0xantman <antho74830@gmail.com>
2021-06-03 16:02:04 +0200

    add og tags

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import './styles/app.css'
+import {Head} from "react-static";
 
 class App extends Component {
   render() {
     return (
         <Suspense fallback={<div>Loading... </div>}>
             <Root>
+                <Head>
+                    <meta charSet="UTF-8" />
+                    <title>LoTerra interface</title>
+                    <meta property="og:title" content="LoTerra" />
+                    <meta property="og:image" content="https://loterra.io/loterra.png" />
+                    <meta property="og:image:alt" content="LoTerra icon" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:site_name" content="LoTerra interface" />
+                    <meta property="og:description" content="LoTerra is building a lottery gaming ecosystem thanks smart contracts on Terra blockchain."/>
+                </Head>
               <Navbar/>
               <div className="content">
                   <Routes default />
