08bc394885d7e1336de6b505b61e77fb6c5c46a9
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2021-07-09 00:58:39 -0700

    Add current jackpot value from contract

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,23 +2,10 @@ import tw from "twin.macro";
 
 import Header from "./Header/Header";
 import AppProviders from "./AppProviders";
-/** 
-import BgWave1 from "../assets/background-wave-1.svg";
-import BgWave2 from "../assets/background-wave-2.svg";
-import BgWave3 from "../assets/background-wave-3.svg";
-import BgWave4 from "../assets/background-wave-4.svg";
-*/
 
 export default function Layout({ children }) {
   return (
     <AppProviders>
-      {/** 
-      <div tw="absolute h-96 w-screen overflow-hidden">
-        <BgWave1 tw="absolute bottom-0" />
-        <BgWave2 tw="absolute bottom-0" />
-        <BgWave3 tw="absolute bottom-0" />
-        <BgWave4 tw="absolute bottom-0" />
-      </div>*/}
       <Header />
       {children}
       <footer tw="shadow-inner mt-4">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import tw, { css } from "twin.macro";
 import { TicketIcon, UsersIcon } from "@heroicons/react/outline";
+import { LCDClient, WasmAPI } from "@terra-money/terra.js";
+import { useEffect, useState, useCallback } from "react";
 
 import Layout from "../components/Layout";
 import Button from "../components/Button";
@@ -13,6 +15,41 @@ const backgroundStyles = css`
 `;
 
 export default function Home() {
+  const [jackpotTotalReward, setJackpotTotalReward] = useState(0);
+
+  const fetchContractQuery = useCallback(async () => {
+    console.log(
+      "process.env.LOTTERY_ADDRESS_V2",
+      process.env.LOTTERY_ADDRESS_V2
+    );
+    const terra = new LCDClient({
+      URL: "https://lcd.terra.dev/",
+      chainID: "columbus-4",
+    });
+    const api = new WasmAPI(terra.apiRequester);
+    try {
+      const contractConfigInfo = await api.contractQuery(
+        process.env.NEXT_PUBLIC_LOTTERY_ADDRESS_V2,
+        {
+          config: {},
+        }
+      );
+      const contractJackpotInfo = await api.contractQuery(
+        process.env.NEXT_PUBLIC_LOTTERY_ADDRESS_V2,
+        {
+          jackpot: { lottery_id: contractConfigInfo.lottery_counter - 1 },
+        }
+      );
+      setJackpotTotalReward(parseInt(contractJackpotInfo) / 1000000);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchContractQuery();
+  }, [fetchContractQuery]);
+
   return (
     <>
       <Head>
@@ -26,7 +63,7 @@ export default function Home() {
           <div tw="flex justify-center">
             <div tw="w-full overflow-x-hidden relative">
               <div tw="px-4 sm:px-6 lg:px-8 z-10">
-                <Jackpot label="Jackpot" amount={126257.94} />
+                <Jackpot label="Jackpot" amount={jackpotTotalReward} />
                 <div tw="flex justify-center mt-4">
                   <StatCard label="Tickets sold" amount={3426366}>
                     <div tw="h-16 w-16 rounded-full text-green-light">
