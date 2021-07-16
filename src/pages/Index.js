6b9f043848f2c9fd7db8904ad14bbd32cb266833
0xantman <antho74830@gmail.com>
2021-07-16 21:02:29 +0200

    Update the contract version

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -154,7 +154,7 @@ export default () => {
          <div>
              <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
                  <div className="text-3xl">LoTerra</div>
-                 <div>contract-v2</div>
+                 <div>contract-v2.0.1</div>
                  <div className="text-sm">terra14mevcmeqt0n4myggt7c56l5fl0xw2hwa2mhlg0</div>
                  <button onClick={() => multiplier(1)} className="button-glass" style={{color:"deeppink", margin: "10px"}}>Generate combination</button>
                  <button onClick={() => multiplier(10)} className="button-glass" style={{color:"deeppink", margin: "10px"}}>X10</button>
