8464e0b51ebf156bf68cb460336ea700e42d1821
Timboow <tim@yourcreativebrand.nl>
2021-08-04 19:39:20 +0200

    prototype usersidemodal

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useMemo} from "react";
-import { X } from 'phosphor-react'
+import { X, Ticket,UserCircle } from 'phosphor-react'
 let useConnectedWallet = {}
 if (typeof document !== 'undefined') {
     useConnectedWallet = require('@terra-money/wallet-provider').useConnectedWallet
@@ -23,9 +23,32 @@ export default function UserModal(props){
                 <div className="row">
                     { connectedWallet && connectedWallet.walletAddress &&
                         (
-                            <div className="col-12">
-                            <p>{connectedWallet.walletAddress}</p>
-                            </div>
+                            <>
+                                <div className="col-12 mb-2 text-center start">
+                                    <UserCircle size={100} />
+                                </div>
+
+                                <div className="col-12 text-center claim">
+                                    <h4 className="mb-2">Current lottery tickets</h4>
+                                    <ul className="list-group text-left" style={{height:'250px', overflowY:'scroll'}}>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                        <li className="list-group-item"><Ticket size={18} color={'#20FF93'} />abcdef</li>
+                                    </ul>
+                                </div>
+
+                                <div className="col-12 text-center mt-4 claim">
+                                    <h4>Claim rewards</h4>
+                                    <p>By clicking this button loterra will check if you won any prizes, if you did we will claim them automatically for you</p>
+                                    <button className="btn btn-special w-100" style={{boxShadow:'none'}}>Claim</button>
+                                </div>
+                            </>
                         )
                     }
                 </div>
