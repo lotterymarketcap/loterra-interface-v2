370fd641f0812f8d32ee673810a0d90eecb7fdc8
Timboow <tim@yourcreativebrand.nl>
2021-08-06 12:09:43 +0200

    better ticket modal

--- src/components/TicketModal.jsx
+++ src/components/TicketModal.jsx
@@ -4,19 +4,21 @@ import { X, Ticket,UserCircle } from 'phosphor-react'
 
 export default function TicketModal(props){
 
-    const { open, combo, toggleModal } = props;
+    const { open, combo, toggleModal, amount } = props;
 
     return (
         <>
         <div className={open ? 'ticketmodal show' : 'ticketmodal'}>
             <div className="ticketmodal_heading text-center">
-                <h2>Edit ticket codes</h2>
+                <h2>Personalize tickets</h2>
+                <p>Rather have your own codes, you can edit your codes to your wishes and buy them right away</p>
             </div>
             <div className="ticketmodal_content">
                 <ul className="list-group">
             {combo && combo.split(" ").map((obj,k) => {
                           return (
-                            <li className="list-group-item" key={k}>{obj && Array.from(obj).map( c => {
+                            <li className="list-group-item" key={k}><Ticket size={24} color={'#4EDC97'} style={{marginRight:'5px'}}/>
+                            {obj && Array.from(obj).map( c => {                                
                                 return (
                                     <input defaultValue={c} className="form-control text-center" />
                                 )
@@ -24,6 +26,7 @@ export default function TicketModal(props){
                           )
                         })}    
                         </ul>
+                        <button className="btn btn-special w-100 my-3">Buy {amount} Tickets</button>
             </div>
         </div>
         <div className={open ? 'backdrop show' : 'backdrop'} onClick={() => toggleModal()}></div>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -334,7 +334,7 @@ export default () => {
                         <input type="number" className="form-control mt-3" value={amount} min="1" max="200" step="1" onChange={(e) => inputChange(e)} />
                         <p className="my-2">Total: {numeral((amount * price) / 1000000).format("0,0.00")} UST</p>
                         <div className="text-sm">{result}</div>
-                        <TicketModal open={ticketModal} combo={combo} toggleModal={() => setTicketModal(!ticketModal)}/>
+                        <TicketModal open={ticketModal} combo={combo} amount={amount} toggleModal={() => setTicketModal(!ticketModal)}/>
                         <button onClick={() => setTicketModal(!ticketModal)} className="btn btn-special-outline w-100 mb-3">Edit ticket codes</button>
                         <button onClick={()=> execute()} className="btn btn-special w-100" disabled={amount <= 0}>Buy {amount} tickets</button>
                       </div>
