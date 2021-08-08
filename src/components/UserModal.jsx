c6643389de15d12f2f9d5dc2fc86b008fe51cde4
0xantman <antho74830@gmail.com>
2021-08-09 01:32:14 +0200

    Fix claim

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -21,7 +21,7 @@ export default function UserModal(props){
     const isPlayer = store.state.allPlayers.includes(connectedWallet.walletAddress);
     const isWinner = store.state.allWinners.includes(connectedWallet.walletAddress);
     
-    const timeStampHalf = store.state.config.block_time_play - (store.state.config.every_block_time_play / 2);
+    const timeStampHalf = (store.state.config.block_time_play * 1000) - (store.state.config.every_block_time_play / 2);
 
     const addToGas = 5300
     const obj = new StdFee(600_000, { uusd: 90000 + addToGas })
