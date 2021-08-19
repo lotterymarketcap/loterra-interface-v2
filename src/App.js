a3b44a2cfe26ed5c9bc714e35c008063665876da
0xantman <antho74830@gmail.com>
2021-08-19 17:52:39 +0200

    Update twitter tag

--- src/App.js
+++ src/App.js
@@ -50,9 +50,9 @@ class App extends Component {
                         <meta property="og:description" content="LoTerra is a lottery contract, buy tickets as a player or join the governance! DAO allows making decisions together! Manage the casino 🎰 Set the prize 🏆 Up the ticket price or go cheap 🏷 Extract max profits 🤑 Keep the vault secure at all times!"/>
                         <meta name="twitter:card" content="summary" />
                         <meta name="twitter:site" content="LoTerra" />
-                        <meta name="twitter:title "content="LoTerra" />
+                        <meta name="twitter:title "content="LoTerra - Decentralized lottery on Terra blockchain" />
                         <meta name="twitter:description" content="LoTerra is a lottery contract, buy tickets as a player or join the governance! DAO allows making decisions together! Manage the casino 🎰 Set the prize 🏆 Up the ticket price or go cheap 🏷 Extract max profits 🤑 Keep the vault secure at all times!"/>
-                        <meta name="twitter:image" content="https://loterra.io/loterra.png"
+                        <meta name="twitter:image" content="https://loterra.io/loterra.png"/>
                     </Head>
                     <StoreProvider>
                   <Navbar/>
