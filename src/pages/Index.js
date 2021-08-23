07f8a91e546d2bcdd83aec83637d87b50aa03add
0xantman <antho74830@gmail.com>
2021-08-23 04:01:53 +0200

    Fix issue

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -288,7 +288,6 @@ export default () => {
             'd',
             'e',
             'f',
-            'g'
         ]
         let randomCombination = ''
         for (let x = 0; x < 6; x++) {
