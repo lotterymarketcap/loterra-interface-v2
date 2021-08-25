d62f2383462f04f1b37f6d383885ce34fa7dfec3
Timboow <tim@yourcreativebrand.nl>
2021-08-25 23:12:07 +0200

    first base for combo text

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -411,12 +411,32 @@ export default () => {
           }
       })
       const ranksArray = [rank4,rank3,rank2,rank1]
-      const comboTexts = [];
+      const comboTexts = [
+        {rank: 1, text:'Perfect!', amount: 1 ,class:''},
+        {rank: 2, text:'Unstoppable!', amount: 1,class:''},
+        {rank: 3, text:'Boom!', amount: 1,class:''},
+        {rank: 4, text:'Good work!', amount: 1,class:''},
+        {rank: 4, text:'Smashed!', amount: 2,class:'medium-text'},
+        {rank: 4, text:'Wipeout!', amount: 3,class:'special-text'},
+        {rank: 4, text:'Boom!', amount: 4,class:'special-text'},
+        {rank: 4, text:'Unstoppable!', amount: 5,class:'super-special-text'},
+      ];
+
+      function comboTextResponse(rank,amount){
+        for (let index = 0; index < comboTexts.length; index++) {
+          const element = comboTexts[index];
+            if(parseInt(rank) == element.rank && amount == element.amount ){
+              return ('<span class="combo-text '+element.class+'">'+element.text+'</span>')
+            }          
+        }          
+      }
+
       let html = '';
       for (let index = 0; index < ranksArray.length; index++) {
         const element = ranksArray[index];
           if(element.length > 0){
             html += '<span class="main">'+element[0]+'</span>' + '<span class="special">x'+element.length+'</span>'
+            html += comboTextResponse(element[0],element.length)
           }
       }     
       
