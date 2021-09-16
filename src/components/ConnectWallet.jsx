a80b1c67c80f797f6f19cbdbcccf86cf0087ca70
Timboow <tim@yourcreativebrand.nl>
2021-09-16 11:13:52 +0200

    commented testdata

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -85,12 +85,9 @@ export default function ConnectWallet(){
           {
             config: {},
           }
-        );     
-      
+        );          
 
-            dispatch({type: "setConfig", message: contractConfigInfo})
-        
-
+        dispatch({type: "setConfig", message: contractConfigInfo})       
         dispatch({type: "setCurrentLotteryId", message: contractConfigInfo.lottery_counter})
         dispatch({type: "setHolderPercentageFee", message: contractConfigInfo.token_holder_percentage_fee_reward})  
 
@@ -218,9 +215,9 @@ export default function ConnectWallet(){
          let recentWinners = state.allRecentWinners;
 
          //Test purposes
-         recentWinners = [
-             {address:"terra1an23yxwkfda0m5dmkcxpyrqux83cw5esg9ex86",claims:{claimed:true,ranks:[4]}},
-            ]
+        //  recentWinners = [
+        //      {address:"terra1an23yxwkfda0m5dmkcxpyrqux83cw5esg9ex86",claims:{claimed:true,ranks:[4]}},
+        //     ]
          
          if(recentWinners.length == 0){
              type = false
