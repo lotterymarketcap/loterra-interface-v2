f60caedcd3dc548002af924902174245468b9c04
Timboow <tim@yourcreativebrand.nl>
2021-10-20 17:10:02 +0200

    new update dogether

--- src/components/Dogether/Main.jsx
+++ src/components/Dogether/Main.jsx
@@ -9,7 +9,7 @@ export default function Main() {
     const { state, dispatch } = useStore()
 
     const [amount, setAmount] = useState(0)
-    const [percentage, setPercentage] = useState(50)
+    const [percentage, setPercentage] = useState(100)
 
     const onSlideChange = (render, handle, value, un, percent) => { 
         setPercentage(percent[0].toFixed(2))
@@ -21,21 +21,25 @@ export default function Main() {
 
     return (
             <>
-                 <strong className="w-100 text-end d-block mt-2"
-                        style={{ textDecoration: 'underline', fontSize:'13px', opacity: 0.6 }}
-                        onClick={() =>
-                             setAmount(state.ustBalance)                            
-                        }
-                    >
-                        MAX: {state.ustBalance ? state.ustBalance.toFixed(2) : 0} UST                       
-                    </strong>
+                
+                <p class="input-heading">The amount you want to stake</p>
+                <span
+                    className="info"
+                    style={{
+                        color: '#ff36ff',
+                        borderColor: '#ff36ff',
+                    }}
+                >
+                    <Info size={14} weight="fill" className="me-1" />
+                    Tickets will be <strong>automatically bought</strong> for the LoTerra Lottery when there is yield available, if you have a <strong>winning ticket</strong> prizes will be deposited automatically to your wallet.
+                </span>
                                 <div className="input-group mb-3">
                                     <span className="input-group-text" id="basic-addon1">
                                         <img src="/UST.svg" width="30px" className="img-fluid"/>
                                     </span>
                                     <input type="number" className="form-control amount-input-staking" onChange={(e) => setAmount(e.target.value)} value={amount} autoComplete="off" placeholder="0.00" name="amount"/>
                                 </div>
-                                <Nouislider className="slider-round"
+                                {/* <Nouislider className="slider-round"
                                 connect={[true, false]}
                                 start={percentage}
                                 onSlide={onSlideChange}
@@ -49,37 +53,30 @@ export default function Main() {
                                 values: [0,50, 100],
                                 density: 4
                                 }}
-                                />
+                                /> */}
         <div className="dogether-settings-info">
             <div className="row">
-            <div className="col-12">
-                        <p>Average dogether stats</p>
+            <div className="col-12 text-center">
+                        <p className="title">Your Dogether Predictions</p>
                     </div>            
-                <div className="col-4 mb-3">
-                <div className="card stats-card">
-                        <div className="card-body">
-                        <small className="d-block">PERCENTAGE DOGETHER</small>
-                    <h4>{percentage}%</h4>
-                    </div>
-                    </div>
-                </div>
-                <div className="col-4 mb-3">
+            
+                <div className="col-6 mb-3">
                     <div className="card stats-card">
                         <div className="card-body">
-                        <small className="d-block">NR TICKETS A DRAW</small>
-                        <h4><Ticket size={30} color={'#ff36ff'} style={{position:'relative',top:'-3px',marginRight:'4px'}}/>{(amount / 100 * percentage / 100 * 20 / 356 * 3).toFixed(2)}</h4>
+                        <small className="d-block">NR TICKETS A WEEK</small>
+                        <h4><Ticket size={30} color={'#82f3be'} style={{position:'relative',top:'-3px',marginRight:'4px'}}/>{(amount / 100 * percentage / 100 * 20 / 356 * 7).toFixed(2)}</h4>
                         </div>
                     </div>
                 </div>
-                <div className="col-4 mb-3">
+                <div className="col-6 mb-3">
                     <div className="card stats-card">
                         <div className="card-body">
                         <small className="d-block">NR TICKETS A YEAR</small>
-                        <h4><Ticket size={30} color={'#ff36ff'} style={{position:'relative',top:'-3px',marginRight:'4px'}}/>{(amount / 100 * percentage / 100 * 20 / 1).toFixed(2)}</h4>
+                        <h4><Ticket size={30} color={'#82f3be'} style={{position:'relative',top:'-3px',marginRight:'4px'}}/>{(amount / 100 * percentage / 100 * 20 / 1).toFixed(2)}</h4>
                         </div>
                     </div>
                 </div>
-                <div className="col-12 my-3">     
+                {/* <div className="col-12 my-3">     
                 <div className="row">
                     <div className="col-12">
                         <p style={{color:'#82f3be'}}>+ Average profits</p>
@@ -109,13 +106,37 @@ export default function Main() {
                     </div>
                 </div>
                 </div>
-                </div>
+                </div> */}
+
+<div className="col-6">
+    <button className="btn btn-normal-lg w-100 mt-4" onClick={(e) => doGether()}>Stake UST</button>
+    <strong className="w-100 text-end d-block mt-2"
+                        style={{ textDecoration: 'underline', fontSize:'13px', opacity: 0.6 }}
+                        onClick={() =>
+                             setAmount(state.ustBalance)                            
+                        }
+                    >
+                        MAX: {state.ustBalance ? state.ustBalance.toFixed(2) : 0} UST                       
+                    </strong>
+</div>
+<div className="col-6">
+    <button className="btn btn-plain-lg w-100 mt-4" onClick={(e) => doGether()}>Unstake UST</button>
+    <strong className="w-100 text-end d-block mt-2"
+                        style={{ textDecoration: 'underline', fontSize:'13px', opacity: 0.6 }}
+                        onClick={() =>
+                             setAmount(state.ustBalance)                            
+                        }
+                    >
+                        MAX: ?? UST                       
+                    </strong>
+</div>
+
             </div>
             {/* <p>{amount}</p>
             <p>{percentage}</p> */}
             
         </div>
-        <button className="btn btn-special w-100 mt-4" onClick={(e) => doGether()}>Add UST to Dogether</button>
+        
             </>
     )
 }
--- src/pages/Dogether.js
+++ src/pages/Dogether.js
@@ -1,15 +1,5 @@
 import React,{useState} from 'react'
-import ProposalItem from '../components/ProposalItem'
-import { useStore } from '../store'
-import { StdFee } from '@terra-money/terra.js'
 import Footer from '../components/Footer'
-import BodyLoader from '../components/BodyLoader'
-import { Bank, Check, Info, Ticket } from 'phosphor-react'
-
-
-
-import numeral from 'numeral'
-import Personal from '../components/Dogether/Personal'
 import Main from '../components/Dogether/Main'
 
 export default () => {   
@@ -34,82 +24,25 @@ export default () => {
                 
             >
               
+                       <div className="container-fluid">
+                           <div className="row">
+                               <div className="col-12">
+                               <img src="/dogether-5.png" className="img-fluid dogether-logo"/>
+                               </div>
+                           </div>
+                       </div>
+                    
                 <div className="container h-100">
                 
                     <div className="row">
-                        <div className="col-12 col-md-8 mx-auto order-1 order-lg-2 p-lg-5">
-                        <img src="/dogether-3.png" className="img-fluid" 
-                style={{
-                    margin:'0 auto',               
-                    display:'block',
-                    position:'relative',
-                }}
-                />
-                            <div className="card lota-card staking" style={{marginTop:'-25px'}}>  
+                     
+                        <div className="col-12 col-md-6 mx-auto">
+                      
+                            <div className="card lota-card staking dogether-card">  
            
                               
-                                <div className="card-body">
-                               
-                                <div className="row">
-                    <div className="col-12 text-center">
-                        <ul
-                            className="nav nav-pills nav-fill mb-3"
-                            id="pills-tab"
-                            role="tablist"
-                        >
-                            <li className="nav-item" role="presentation">
-                                <button
-                                    className="nav-link active"
-                                    id="pills-staking-tab"
-                                    data-bs-toggle="pill"
-                                    data-bs-target="#pills-staking"
-                                    type="button"
-                                    role="tab"
-                                    aria-controls="pills-staking"
-                                    aria-selected="true"
-                                >
-                                    Dogether
-                                </button>
-                            </li>
-
-                            <li className="nav-item" role="presentation">
-                                <button
-                                    className="nav-link"
-                                    id="pills-lpstaking-tab"
-                                    data-bs-toggle="pill"
-                                    data-bs-target="#pills-lpstaking"
-                                    type="button"
-                                    role="tab"
-                                    aria-controls="pills-lpstaking"
-                                    aria-selected="false"
-                                >
-                                    Your Dogether
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="tab-content" id="pills-tabContent">
-                        <div
-                            className="tab-pane fade show active"
-                            id="pills-staking"
-                            role="tabpanel"
-                            aria-labelledby="pills-staking-tab"
-                        >
-                            <Main/>
-                        </div>
-                        <div
-                            className="tab-pane fade"
-                            id="pills-lpstaking"
-                            role="tabpanel"
-                            aria-labelledby="pills-lpstaking-tab"
-                        >
-                            <Personal/>
-                        </div>
-                    </div>
-                  
-                </div>
-                               
-                           
+                                <div className="card-body">                               
+                                    <Main/>                              
                                 </div>
                             </div>
                         </div>
